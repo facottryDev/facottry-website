d08930738ab132d0766e5bb92f35716cb8641ca6
Styles <bcs_2021035@iiitm.ac.in>
2024-02-07 13:13:13 +0530

    minor changes

--- src/components/dashboard/Home.tsx
+++ src/components/dashboard/Home.tsx
@@ -84,36 +84,16 @@ const Dashboard = () => {
   
           <hr className="w-full mt-4" />
   
-          <div className="flex justify-between mt-8 space-x-8">
+          <div className="flex flex-col justify-between mt-8 space-x-8">
+            {/* Filters */}
+            <div>
+            </div>
+
             {/* Theme Selector Panel */}
             <div className="w-full h-fit p-8 space-y-4 bg-white rounded-lg dark:bg-darkblue">
               <Accordion title="Player" themes={PlayerThemes} />
               <Accordion title="Home" themes={HomeThemes} />
-            </div>
-  
-            {/* Config Selection Panel */}
-            <div className="flex flex-col bg-white w-full max-w-[240px] rounded-lg h-fit min-h-[50vh] px-5 dark:bg-darkblue">
-              <h1 className="mt-4 text-xl font-bold">Configs</h1>
-              <hr className="w-full mt-2" />
-  
-              {
-                ConfigList.map((config, index) => (
-                  <div key={index} className="flex items-center mt-4 mb-2 cursor-pointer">
-                    <input id={config} type="radio" value="" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 cursor-pointer focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-  
-                    <label htmlFor={config} className="ml-2 text-sm font-medium text-gray-900 cursor-pointer dark:text-gray-300">{config}</label>
-                  </div>
-                ))
-              }
-  
-              <hr className="w-full mt-4" />
-  
-              <button className="flex items-center gap-1 p-2 my-4 font-medium text-white transition-all rounded-lg w-fit bg-primary hover:bg-primary400">
-                <AiOutlinePlus fontSize='1.3rem' />
-                <p>Add Config</p>
-              </button>
-            </div>
-  
+            </div>       
           </div>
         </div>
       </div>
--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -4,7 +4,13 @@ const axios_auth = axios.create({
   baseURL: process.env.NEXT_PUBLIC_AUTH_BASE_URL,
 });
 
+const axios_server = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_DASHBOARD_BASE_URL,
+});
+
 axios_auth.defaults.withCredentials = true;
 axios_auth.defaults.headers.post["Content-Type"] = "application/json";
+axios_server.defaults.withCredentials = true;
+axios_server.defaults.headers.post["Content-Type"] = "application/json";
 
 export { axios_auth };
