43ac2f98110b0fd60984f30d9dedd6395ee74295
Styles <bcs_2021035@iiitm.ac.in>
2024-05-30 17:26:19 +0530

    google oauth working

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -72,7 +72,7 @@ const SignupForm = () => {
 
                         <button type="submit" className="mb-4 w-full bg-primary bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:bg-slate-400 dark:hover:bg-slate-300 transition-all text-white hover:bg-primary/80 dark:text-black">Continue</button>
 
-                        <Link href='/google' className="flex items-center justify-center mb-4 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-slate-700 dark:focus:ring-primary-800 border dark:border-slate-400 transition-all hover:bg-slate-100 dark:text-slate-300">
+                        <Link href={process.env.NEXT_PUBLIC_AUTH_BASE_URL + '/google'} type="button" className="flex items-center justify-center mb-4 w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-slate-700 dark:focus:ring-primary-800 border dark:border-slate-400 transition-all hover:bg-slate-100 dark:text-slate-300">
                             <Image src={GoogleIcon} alt="google icon" width={20} height={20} className="inline-block mr-2" />
                             <p className="text-black dark:text-slate-300">Continue with Google</p>
                         </Link>
